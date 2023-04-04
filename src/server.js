8b4dad770983901cfe0fc750166dbd302b0ff2c0
Angie <angelicarlos@icloud.com>
2023-04-04 21:01:59 +1000

    add firebase admin

--- src/server.js
+++ src/server.js
@@ -1,12 +1,36 @@
+import fs from "fs";
 import express from "express";
-import { db, connecToDB } from "./db.js"
+import { db, connecToDB } from "./db.js";
+import admin from "firebase-admin";
+
+const credentials = JSON.parse(
+    fs.readFileSync('credentials.json')
+);
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
+app.use(async(req, res, next)=>{
+    const { authtoken } = req.headers;
+
+    if (authtoken){
+        try{
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch(error){
+            res.sendStatus(400);
+        }
+    }
+
+    next();
+});
 
 app.get('/api/articles/:name', async(req, res)=>{
     const { name } = req.params;
+    const { uid } = req.user;
 
     await db.collection('articles').updateOne({ name },
         { $inc: { pageViews: 1 },
@@ -15,28 +39,43 @@ app.get('/api/articles/:name', async(req, res)=>{
     const article = await db.collection('articles').findOne({ name });
 
     if(article){
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     }
     else{
         res.sendStatus(404);
     }
-
     
+});
+
+app.use((req, res, next)=>{
+    if (req.user) {
+        next();
+    } else{
+        res.sendStatus(401);
+    }
 });
 
 app.put('/api/articles/:name/upvote', async(req, res)=>{
     const { name } = req.params;
-
-    await db.collection('articles').updateOne({ name },
-        { $inc: { upvotes: 1 },
-    });
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if(article){
-        res.json(article);
-    }
-    else{
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.include(uid);
+        
+        if(canUpvote){
+            await db.collection('articles').updateOne({ name },
+                { $inc: { upvotes: 1 },
+                $push: { upvoteIds: uid }
+            });
+        }
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle);
+    } else{
         res.sendStatus(404);
     }
 
